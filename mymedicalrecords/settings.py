fc750fb51e932a71f1816abc377e38b8432db23d
sohail8611 <82253012+sohail8611@users.noreply.github.com>
2021-04-24 10:20:23 -0700

    Update settings.py

--- mymedicalrecords/settings.py
+++ mymedicalrecords/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^!u1l9lavkmg&p2wkwt%((sltqq6tr@&571y1o0v5j3n(e*1dm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['docdashboard.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['docdashboard.herokuapp.com']
+server {
+    listen 80 default_server;
+    return 444;
+}
 
 
 # Application definition
