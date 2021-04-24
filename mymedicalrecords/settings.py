1ad72094f5c19b67a5df2ac7494dd67aa3196cc1
sohail8611 <82253012+sohail8611@users.noreply.github.com>
2021-04-24 10:34:26 -0700

    Update settings.py

--- mymedicalrecords/settings.py
+++ mymedicalrecords/settings.py
@@ -25,44 +25,7 @@ SECRET_KEY = '^!u1l9lavkmg&p2wkwt%((sltqq6tr@&571y1o0v5j3n(e*1dm'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
-                       'pathname=%(pathname)s lineno=%(lineno)s '
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
+DEBUG = True
 
 ALLOWED_HOSTS = ['docdashboard.herokuapp.com']
 
